b30525133e67375aed0a8de00d827808b00ee740
Angel Diaz <upper.venezuela@gmail.com>
2022-04-05 09:21:27 -0400

    counte reparador

--- src/controllers/fixer.ctrl.js
+++ src/controllers/fixer.ctrl.js
@@ -3,6 +3,8 @@ const { Op } = require("sequelize");
 
 async function fixerPermissions(req,res){
     const t = await model.sequelize.transaction();
+    let created=0;
+    let updated=0;
     await model.role.findAll().then(async function(rsRole){        
         for (let index = 0; index < rsRole.length; index++) { // recorre todos lo roles            
             if(rsRole[index].id!=5){
@@ -10,24 +12,25 @@ async function fixerPermissions(req,res){
                     for (let j = 0; j < rsPermission.length; j++) {                       
                         try{
                             await model.grantRole.create({roleId:rsRole[index].id,permissionId:rsPermission[j].id,isActived:false},{transaction:t})
+                            created++;
                         }catch(error){
                             console.log("Role:",rsRole[index].id,"Permision:",rsPermission[j].id);
-                            if(error.name=="SequelizeDatabaseError"){
+                            
                                 console.log("Actualizando");
                                 await model.grantRole.update({roleId:rsRole[index].id,permissionId:rsPermission[j].id,isActived:false},
                                     {where:{roleId:rsRole[index].id,permissionId:rsPermission[j].id}},{transaction:t})
-                            }else{                                
-                                console.log(error.name);                                                           
+                                    updated++;                                                
+                                //console.log(error.name);                                                           
                                 //res.status(403).json({data:{"result":false,"message":error.message}});
                                 //res.end();
-                            }
+                            
                         }                                         
                     }
                 })
             }
         }
         t.commit();
-        res.status(200).json({data:{"result":true,"message":"Reparación exitosa"}});
+        res.status(200).json({data:{"result":true,"message":"Reparación exitosa","data":{"creados":created,"Actualizados":updated}}});
     })/*.catch(async function(error){
         t.rollback();
         console.log(error);
