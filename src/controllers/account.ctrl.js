8055e5845aa782e0d25cd5aa0b42c4e25b79611c
Angel Diaz <upper.venezuela@gmail.com>
2022-04-22 09:02:48 -0400

    login img delete

--- src/controllers/account.ctrl.js
+++ src/controllers/account.ctrl.js
@@ -198,7 +198,7 @@ async function loginAccount(req,res){
                                         }								
                                         dataAccount={"id":rsUser.id,"name":rsUser.name,"email":rsUser.email} //Datos de la cuenta	
                                         dataPeople=rsUser.people;	
-                                        dataPeople.photo=rsUser['employeeFiles'][0].photo;
+                                        //dataPeople.photo=rsUser['employeeFiles'][0].photo;
                                         var token =  await serviceToken.newToken(dataAccount,allRole,'login',dateTime,dataPeople) //generar Token 									
                                         await model.account.update({tries:0},{where:{id:rsUser.id}}).then(async function(rsNewPassword){ //Actualiza tries
                                             res.status(200).json({data:{"result":true,"message":"Usted a iniciado sesión " + rsUser.email ,"token":token,tokenRole,"people":dataPeople,"account":dataAccount,"role":allRole}});        
