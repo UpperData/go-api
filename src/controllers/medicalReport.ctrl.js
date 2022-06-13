773df0b41549f65c32e66147c2397285301009bc
Upper Data Solutions C.A <upper.venezuela@gmail.com>
2022-06-13 15:22:55 +0200

    fix

--- src/controllers/medicalReport.ctrl.js
+++ src/controllers/medicalReport.ctrl.js
@@ -51,6 +51,7 @@ async function medicalReportGetByDoctor(req,res){
                 include:[
                     {
                         model:model.appointment,
+                        attributes:[id,medialPersonal],
                         where:{
                             medialPersonal:{
                                 doctor:{
