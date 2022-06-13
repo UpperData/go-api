584db44f748e28de18ce15bc419ea4e20cabe307
Upper Data Solutions C.A <upper.venezuela@gmail.com>
2022-06-13 14:59:30 +0200

    general fix

--- src/routes/medicalReport.route.js
+++ src/routes/medicalReport.route.js
@@ -7,5 +7,5 @@ const forceBrute=require('../controllers/middleware/noBrute.ctrl');
 router.get('/MEdiCaL/GET/Report/:id',forceBrute.notBruteSecure,auth.autorizedRole([5]),report.medicalReportGet); //Obtiener un informe medico
 router.post('/MEDiCAl/aDd/REport/',forceBrute.notBruteSecure,auth.autorizedRole([5]),report.medicalRepostNew); //registra un informe medico
 router.put('/mEdiCaL/EDiT/RepORt/',forceBrute.notBruteSecure,auth.autorizedRole([5]),report.medicalReportEdit); //edita un informe medico
-router.put('/mEdiCAL/get/RePORt/DOCtOR/:id',forceBrute.notBruteSecure,auth.autorizedRole([5]),report.medicalReportEdit); //edita un informe medico
+router.put('/mEdiCAL/get/RePORt/DOCtOR/:id',forceBrute.notBruteSecure,auth.autorizedRole([5]),report.medicalReportGetByDoctor); //edita un informe medico
 module.exports=router;
