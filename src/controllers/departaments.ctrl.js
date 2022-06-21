107511b52dce9fd9a6178698225bcc1724654bda
Upper Data Solutions C.A <upper.venezuela@gmail.com>
2022-06-21 16:52:30 +0200

    fix error

--- src/controllers/departaments.ctrl.js
+++ src/controllers/departaments.ctrl.js
@@ -79,7 +79,7 @@ async function createCargo(req,res){
             [model.sequelize.fn('max', model.sequelize.col('id')), 'max_id']]
     });
     console.log(id);
-    return await model.cargo.create({id:id.max_id+1,departamentId,name},{transaction:t}).then(async function(rsDepartament){
+    return await model.cargo.create({id:id.dataValues.max_id+1,departamentId,name},{transaction:t}).then(async function(rsDepartament){
         t.commit()
         res.status(200).json({"data":{"result":true,"message":"Registro Satisfactorio","data":rsDepartament}});      
     }).catch(async function(error){  
