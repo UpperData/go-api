4158c3d1b2c060ae6c5af5674ac4be2643ec28af
Upper Data Solutions C.A <upper.venezuela@gmail.com>
2022-06-21 16:39:58 +0200

    update cargo

--- src/controllers/departaments.ctrl.js
+++ src/controllers/departaments.ctrl.js
@@ -74,7 +74,11 @@ async function editSubDepartament(req,res){
 async function createCargo(req,res){ 
     const{departamentId,name}=req.body;
     const t = await model.sequelize.transaction();
-    return await model.cargo.create({departamentId,name},{transaction:t}).then(async function(rsDepartament){
+    const id=await model.cargo.findOne({
+        attributes:[
+            [model.sequelize.fn('max', model.sequelize.col('id')), 'max_id']]
+    });
+    return await model.cargo.create({id:id.max_id+1,departamentId,name},{transaction:t}).then(async function(rsDepartament){
         t.commit()
         res.status(200).json({"data":{"result":true,"message":"Registro Satisfactorio","data":rsDepartament}});      
     }).catch(async function(error){  
