f38119a370831bcb9125569776031ce7f25ad523
Upper Data Solutions C.A <upper.venezuela@gmail.com>
2022-06-22 14:05:42 +0200

    tipo de cambio

---
+++ src/controllers/changeType.ctrl.js
@@ -0,0 +1,53 @@
+const model=require('../db/models/index');
+async function getChangeType(req,res){
+    const {id}=req.params;
+    if(id!='*'){
+        //Busca un modulo
+        return await model.changeType.findOne({
+            where:{id}
+        }).then(async function(rsChangeType){
+            if(rsChangeType){
+                res.status(200).json({"data":{"result":true,"message":"Busqueda satisfatoria","data":rsChangeType}});        
+            }else{
+                res.status(403).json({"data":{"result":false,"message":"No existe registro con este código"}});            
+            }
+            
+        }).catch(async function(errror){
+            res.status(403).json({"data":{"result":false,"message":"Algo salió mal buscando registro"}});        
+        })
+    }else{
+        //Busca todos los roles
+        return await model.changeType.findAll({order:['updateAt']}).then(async function(rsChangeType){
+            res.status(200).json({"data":{"result":true,"message":"Busqueda satisfatoria","data":rsChangeType}});        
+        }).catch(async function(errror){
+            res.status(403).json({"data":{"result":false,"message":"Algo salió mal buscando registro"}});        
+        })
+    }
+}
+async function getCurrentChangeType(req,res){
+    return await model.changeType.findOne({
+        attributes:[ [model.sequelize.fn('max', model.sequelize.col('id')), 'max_id'],'value']
+    }).then(async function(rsChangeType){
+        if(rsChangeType){
+            res.status(200).json({"data":{"result":true,"message":"Busqueda satisfatoria","data":rsChangeType}});        
+        }else{
+            res.status(403).json({"data":{"result":false,"message":"No existe registro con este código"}});            
+        }
+        
+    }).catch(async function(errror){
+        res.status(403).json({"data":{"result":false,"message":"Algo salió mal buscando registro"}});        
+    })
+}
+async function createChangeType(req,res){ 
+    const{value}=req.body;
+    const t = await model.sequelize.transaction();
+    return await model.changeType.create({value},{transaction:t}).then(async function(rsChangeType){
+        t.commit()
+        res.status(200).json({"data":{"result":true,"message":"Registro Satisfactorio","data":rsChangeType}});      
+    }).catch(async function(error){  
+        t.rollback()  
+        console.log(error);        
+        res.status(403).json({"data":{"result":false,"message":"Algo salió mal creando registro"}});  
+    })
+}
+module.exports={getCurrentChangeType,getChangeType,createChangeType}
--- src/controllers/departaments.ctrl.js
+++ src/controllers/departaments.ctrl.js
@@ -78,9 +78,6 @@ async function createCargo(req,res){
         attributes:[
             [model.sequelize.fn('max', model.sequelize.col('id')), 'max_id']]
     });
-    console.log(id.dataValues.max_id);
-    console.log(id.max_id);
-    console.log(id.max_id+1);
     return await model.cargo.create({id:id.dataValues.max_id+1,departamentId,name},{transaction:t}).then(async function(rsDepartament){
         t.commit()
         res.status(200).json({"data":{"result":true,"message":"Registro Satisfactorio","data":rsDepartament}});      
