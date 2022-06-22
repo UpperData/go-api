57fea1211b1b690fd41d13f495bcce54fec1e098
Upper Data Solutions C.A <upper.venezuela@gmail.com>
2022-06-22 14:24:43 +0200

    general fix

--- src/routes/changeType.route.js
+++ src/routes/changeType.route.js
@@ -5,6 +5,6 @@ const forceBrute=require('../controllers/middleware/noBrute.ctrl');
 const auth=require('../controllers/middleware/auth.ctrl');
 
 router.post('/change/Type/add',forceBrute.notBruteSecure,auth.autorizedRole(['5']), changeType.createChangeType); // Regsitra tipo de cambio
-
+router.get('/change/Type/get/:id',forceBrute.notBruteSecure,auth.autorizedRole(['*']), changeType.getChangeType); // optiene tipo de cambio
 
 module.exports=router;
