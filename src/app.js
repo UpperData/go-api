baae0df5db2f7507e095bf4f89f15570cac39ebb
Upper Data SOlutions C.A <upper.venezuela@gmail.com>
2023-07-27 09:46:13 -0400

    change port

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ const app =express(); //incializa el framework
 
 //app.set('trust proxy', 1); // trabaja en conjunto con el limite de peticiones a las ruta
 //app.use(apiLimiter); // Limita conexiones
-app.set('port',process.env.PORT || 25109 ); // comunication port
+app.set('port',process.env.PORT || 4094 ); // comunication port
 app.use(helmet()); //ayuda a proteger la aplicación de algunas vulnerabilidades web conocidas mediante el establecimiento correcto de cabeceras HTTP.
 
 // Middleware
