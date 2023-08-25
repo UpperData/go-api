46bca080ea40acdfdbcf059efd99ec0a66c94b7c
Angel <upperdata@gmail.com>
2023-08-24 22:05:22 -0400

    contratos

--- src/controllers/store.ctrl.js
+++ src/controllers/store.ctrl.js
@@ -63,10 +63,83 @@ async function updateStore(req,res){
             res.status(403).json({"data":{"result":false,"message":"Algo salió mal actualizando tienda, intente nuevamente"}});
         });
 }
+async function createContract(req,res){
+    //valida campos requeridos
+    const  {startDate,endDate,comission,storeId,isActived,fileContract}=req.body;
+    const t = await model.sequelize.transaction();
+    await model.contract.findAndCountAll({where:{storeId,isActived:true}})
+    .then(async function(rsFindContract){
+        if(rsFindContract.count>0){
+            t.rollback();
+            res.status(403).json({"data":{"result":true,"message":"Contrano NO procesado, tienda posee un contrato activo "}});
+        }else{
+            await model.contract.create({startDate,endDate,comission,storeId,isActived,fileContract},{transaction:t})
+            .then(async function(rsResult){            t.commit();
+                res.status(200).json({"data":{"result":true,"message":"Contrato registrado satisfactoriamente"}});
+            }).catch(async function(error){
+                console.log(error)
+                t.rollback();
+                res.status(403).json({"data":{"result":false,"message":"Algo salió mal creando contrato, intente nuevamente"}});
+            });
+        }
+    })
+
+}
+async function revokeContract(req,res){
+    const  {id,isActived}=req.body;
+    const t = await model.sequelize.transaction();
+    await model.contract.update({isActived},{where:{id}},{transaction:t})
+        .then(async function(rsContract){
+            t.commit();
+            res.status(200).json({"data":{"result":true,"message":"Contrato actualizado satisfactoriamente"}});
+        }).catch(async function(error){
+            t.rollback();
+            res.status(403).json({"data":{"result":false,"message":"Algo salió mal actualizando contrato, intente nuevamente"}});
+        });
+}
+async function findContract(req,res){
+    const {id}=req.params;
+    if(id!='*'){
+        //Busca una tienda
+        return await model.contract.findOne({
+            where:{id}
+        }).then(async function(rsContract){
+            if(rsContract){
+                res.status(200).json({"data":{"result":true,"message":"Busqueda satisfatoria","data":rsContract}});        
+            }else{
+                res.status(403).json({"data":{"result":false,"message":"No existe registro con este código"}});            
+            }
+            
+        }).catch(async function(errror){
+            res.status(403).json({"data":{"result":false,"message":"Algo salió mal buscando registro"}});        
+        })
+    }else{
+        //Busca todas las tiendas
+        return await model.contract.findAll({order:['createdAt']}).then(async function(rsContract){
+            res.status(200).json({"data":{"result":true,"message":"Busqueda satisfatoria","data":rsContract}});        
+        }).catch(async function(errror){
+            console.log(errror)
+            res.status(403).json({"data":{"result":false,"message":"Algo salió mal buscando registro"}});        
+        })
+    }
+}
+async function findContractByStore(req,res){
+    const {storeId}=req.params;  
+    return await model.contract.findAll({where:{storeId}},{order:['createdAt']}).
+    then(async function(rsContract){
+        res.status(200).json({"data":{"result":true,"message":"Busqueda satisfatoria","data":rsContract}});        
+    }).catch(async function(errror){
+        console.log(errror)
+        res.status(403).json({"data":{"result":false,"message":"Algo salió mal buscando registro"}});        
+    })
+    
+}
 module.exports={
     createStore, // crea una tienda
     findStore,  //buscar tienda
-    updateStore // actualizar tienda
-
-
+    updateStore, // actualizar tienda
+    createContract, // Crea un contrato
+    revokeContract, // Editar contrato
+    findContract,  // contrato x id / all
+    findContractByStore // contratos por tienda
 }
