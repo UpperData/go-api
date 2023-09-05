91fb4a4866af1b8e955042756a9c0b75e67be88e
Angel <upperdata@gmail.com>
2023-09-05 06:29:54 -0400

    category routes

--- src/routes/categories.route.js
+++ src/routes/categories.route.js
@@ -5,8 +5,8 @@ const serviceToken = require('../controllers/serviceToken.ctrl');
 const auth=require('../controllers/middleware/auth.ctrl');
 const forceBrute=require('../controllers/middleware/noBrute.ctrl');
 
-router.get('/front/categoriES/GET',forceBrute.notBruteSecure,auth.autorizedRole(['*']),category.getMainCategories); //Get Main Category
-router.post('/frONT/CAtegorRIES/CREATE',forceBrute.notBruteSecure,auth.autorizedRole(['*']),category.addMainCategories); //add Main caterory
-router.get('/Front/SubCATEGORY/GET/:mainCategoryId',forceBrute.notBruteSecure,auth.autorizedRole(['*']),category.getSubCategories);
-router.post('/FRont/sUBCAterory/CREATE',forceBrute.notBruteSecure,auth.autorizedRole(['*']),category.addSubCategories) //Crea subcategoria
+router.get('/front/categoriES/GET',forceBrute.notBruteSecure,category.getMainCategories); //Get Main Category
+router.post('/frONT/CAtegorRIES/CREATE',forceBrute.notBruteSecure,category.addMainCategories); //add Main caterory
+router.get('/Front/SubCATEGORY/GET/:mainCategoryId',forceBrute.notBruteSecure,category.getSubCategories);
+router.post('/FRont/sUBCAterory/CREATE',forceBrute.notBruteSecure,category.addSubCategories) //Crea subcategoria
 module.exports=router;
