095bbdd937988a89d31c0ea311188df61f084728
Upper Data Solutions C.A <upper.venezuela@gmail.com>
2023-11-21 16:41:56 -0400

    cambios varios

--- src/controllers/shoppingCar.ctrl.js
+++ src/controllers/shoppingCar.ctrl.js
@@ -4,12 +4,25 @@ const serviceToken=require('./serviceToken.ctrl');
 const generals=require('./generals.ctrl');
 
 async function getShoppingCar(req,res){      
-    const{accountId}=req.params  
+    //const{accountId}=req.params  
     const dataToken=await serviceToken.dataTokenGet(req.header('Authorization').replace('Bearer ', ''));         
     if(dataToken){
         //Busca carrito de ocmpra
-        return await model.shoppingCar.findAll({            
-            where:{accountId}
+        return await model.shoppingCar.findAll({    
+            attributes:{exclude:['createdAt','updatedAt']} ,
+            include:[{
+                model:model.account,
+                attributes:['id'],
+                include:[{
+                    model:model.store,
+                    attributes:['id'],
+                    include:[{
+                        model:model.article,
+                        attributes:['id','name']
+                    }]
+                }] 
+            }],
+            where:{accountId:dataToken['account'].id}
         }).then(async function(rsCar){
             if(rsCar){
                 res.status(200).json({"data":{"result":true,"message":"Busqueda satisfatoria","data":rsCar}});        
@@ -25,11 +38,11 @@ async function getShoppingCar(req,res){
     } 
 }
 async function AddShoppingCar(req,res){      
-    const{accountId,items}=req.body  
+    const{items}=req.body  
     const dataToken=await serviceToken.dataTokenGet(req.header('Authorization').replace('Bearer ', ''));         
     if(dataToken){
         //Busca carrito de ocmpra
-        return await model.shoppingCar.create({accountId,items})
+        return await model.shoppingCar.create({accountId:dataToken['account'].id,items})
         .then(async function(rsCar){
             if(rsCar){
                 res.status(200).json({"data":{"result":true,"message":"Item agregado"}});        
@@ -44,4 +57,5 @@ async function AddShoppingCar(req,res){
         res.status(403).json({"data":{"result":false,"message":"Debe inicicar sesión"}});   
     } 
 }
+
 module.exports={getShoppingCar,AddShoppingCar};
